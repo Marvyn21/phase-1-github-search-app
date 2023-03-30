65f66e8ccb1af38c89059f14054d68c10dbc6854
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-03-30 16:53:44 +0300

    Completed Assigment

--- js/index.js
+++ js/index.js
@@ -7,44 +7,45 @@ document.addEventListener("DOMContentLoaded", () => {
 let searchUser = (e) => {
     e.preventDefault();
 
-    const username = document.querySelector("search").value;
+    const username = document.querySelector("#search").value;
     fetch(`https://api.github.com/search/users?q=${username}`)
     .then(res => res.json())
     .then(users => {renderUserDetails(users.items)})
     .catch(error => {console.log("error")});
 }
 
-let renderUserDetails = (users) => {
-    users.forEach(users => {
+let renderUserDetails = users => {
+    users.forEach(user => {
         createUserCard(user)
     });
 } 
 
-let createUserCard = (user) => {
+let createUserCard = user => {
     const userCard = document.createElement("div");
     userCard.className = "user-card";
     document.querySelector("#user-list").appendChild(userCard);
     userCard.innerHTML = `<img src="${user.avatar_url}">`
                         + `<h2>${user.login}</h2>`
                         + `<a href="${user.html_url}" target="_blank"> Go to Github Profile</a><br>`
-                        + `button class="subbmit-btn>View ${user.login}'s Repos</button>`
+                        + `<button class="submit-btn">View ${user.login}'s Repos</button>`
     userCard.querySelector(".submit-btn").addEventListener('click', () => {
         fetchRepositories(user)
     })
 }
 
-let fetchRepositories = (username) => {
-    fetch(`https://api.github.com/users/${username.login}/repos`)
+
+let fetchRepositories = user => {
+    fetch(`https://api.github.com/users/${user.login}/repos`)
     .then(res => res.json())
     .then(repos => {renderRepoDetails(repos)})
     .catch(error => {console.log("Some Error here")});
 }
 
-let renderRepoDetails = (repos) => {
+let renderRepoDetails = repos => {
     repos.forEach(repo => createRepoCard(repo))
 }
 
-let createRepoCard = (repo) => {
+let createRepoCard = repo => {
     const repoCard = document.createElement("div");
     repoCard.className = "repo-card"
     document.querySelector("#repos-list").appendChild(repoCard);
